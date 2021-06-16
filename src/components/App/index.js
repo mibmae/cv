858bc8a0429dbecd040c52ea7e332f347b84c504
mibmae <mibmae@gmail.com>
2021-06-16 11:47:00 +0200

    chat ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,5 @@
 // == Import npm
-import React from 'react';
+import React, { useEffect } from 'react';
 
 // == Import
 import Header from 'src/components/Header';
@@ -12,13 +12,54 @@ import Bandeau from 'src/components/Bandeau';
 import Projets from 'src/components/Projets';
 import Contact from 'src/components/Contact';
 import Infos from 'src/components/Infos';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+import 'react-chat-widget/lib/styles.css';
+import socket from 'src/socketio';
 
 // == Composant
 function App() {
+
+
+  socket.emit('connection');
+
+  socket.on('private_message', (res) => {
+    addResponseMessage(res);
+  });
+
+
+  useEffect(() => {
+    socket.on('private message', (msg) => {
+      addResponseMessage(msg);
+    }, []);
+  });
+
+  // useEffect(() => {
+  //   socket.on('news', (res) => addResponseMessage(res));
+  //   // addResponseMessage('Laissez moi un message, je vous répondrai le plus vite possible.');
+  // }, []);
+
+  const handleNewUserMessage = (newMessage) => {
+    // socket.on('message', newMessage);
+    //  addResponseMessage(newMessage);
+
+    // console.log(`New message incoming! ${newMessage}`);
+    // socket.emit('sendmessage', newMessage);
+    socket.emit('client_message', newMessage);
+    // Now send the message throught the backend API
+  };
+
   return (
     <div className="app">
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        // profileAvatar={logo}
+        title="Me contacter"
+        subtitle="C'est par ici !"
+        senderPlaceHolder="Veuillez entrer un message"
+      />
       <Header />
       <Bandeau />
+      {/* <WeatherWidget city="Saint-ambroix" /> */}
       <Switch>
         <Route path="/" exact>
           <Formations />
---
+++ src/socketio/index.js
@@ -0,0 +1,3 @@
+const socket = window.io('http://localhost:3001/');
+
+export default socket;
